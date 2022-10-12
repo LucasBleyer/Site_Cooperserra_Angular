4dd600638603c7b9fbbc41525cd6229ccb644071
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-10-12 19:53:49 -0300

    home com imagens

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,18 @@
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
   constructor(title: Title) {
     title.setTitle("Cooperserra | Home")
-   }
+  }
 
   ngOnInit(): void {
   }
