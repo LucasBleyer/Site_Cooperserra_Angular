f8b53a485e31413e48677cdc7e7423bcaa37d841
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-10-18 20:38:51 -0300

    informações e curiosidades abrindo com modal na home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
@@ -10,11 +10,18 @@ import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(title: Title) {
+  constructor(title: Title, private modalService: NgbModal) {
     title.setTitle("Cooperserra | Home")
   }
 
   ngOnInit(): void {
   }
 
+  public abrirDiferencas(modalDiferencas: TemplateRef<NgbModal>){
+    this.modalService.open(modalDiferencas);
+  }
+
+  public abrirTabela(modalTabela: TemplateRef<NgbModal>){
+    this.modalService.open(modalTabela);
+  }
 }
